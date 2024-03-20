d8792220f701cf743268d6bea71a099000a2790c
eldarcodes <prog.eldar@gmail.com>
2024-03-20 20:03:28 +0200

    feat: add readme

--- src/big-o/quadratic.ts
+++ src/big-o/quadratic.ts
@@ -1,11 +1,11 @@
-function nestedLoop(numbers: number[]): void {
+export function nestedLoop(numbers: number[]): void {
   for (let i = 0; i < numbers.length; i++) {
     for (let j = 0; j < numbers.length; j++) {
-      console.log(numbers[i], numbers[j]);
+      console.log(i, j);
     }
   }
 }
 
-const numbers = [1, 2, 3, 4];
+const numbers = [1, 2, 3];
 
 nestedLoop(numbers); // O(n^2) --> Quadratic Time
